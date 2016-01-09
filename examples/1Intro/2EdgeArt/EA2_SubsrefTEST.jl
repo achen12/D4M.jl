00928e89c90f8e6769989694954e1789d702c02a
achen12 <chen.alex121@gmail.com>
2016-01-09 15:01:33 +0800

    equals fix

--- examples/1Intro/2EdgeArt/EA2_SubsrefTEST.jl
+++ examples/1Intro/2EdgeArt/EA2_SubsrefTEST.jl
@@ -4,7 +4,7 @@ print(csv_dir)
 E = ReadCSV(csv_dir);                       # Read CSV file into associative array.
 print(E);                                # Show in tabular form.
 
-Eo = E[(E[:,"Color,"] == "Orange," ).row,:];   # Get orange edges.
+Eo = E[(E[:,"Color,"] == "Orange" ).row,:];   # Get orange edges.
 print(Eo);                               # Show in tabular form.
 
 Eog = E[ StartWith("O,G,") ,:];               # Get orange and green edges.
